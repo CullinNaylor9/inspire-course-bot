089f737caf68c10e006db1f8f4b6afdeb6f5593e
CullinNaylor9 <106817419+CullinNaylor9@users.noreply.github.com>
2025-02-26 18:23:40 +0200

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -296,7 +296,7 @@ const features = [
 // Robot showcase images for carousel
 const robotImages = [
   "https://pixeldrain.com/api/file/ftoZQ3Rj",
-  "https://files.catbox.moe/8uhd5i.jpeg",
+  "https://pixeldrain.com/api/file/7rE9RH6y",
   "https://files.catbox.moe/vaye8v.jpeg",
   "https://files.catbox.moe/3ucm97.jpeg",
   "https://files.catbox.moe/je2hbj.jpeg",
